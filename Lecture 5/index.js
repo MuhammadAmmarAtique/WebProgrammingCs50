3cd3a4efbaf0ff72cdb0fb73f2033ab76f6d382c
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-09-25 20:21:21 +0500

    Developed a toggle function.

--- Lecture 5/index.js
+++ Lecture 5/index.js
@@ -1,9 +1,27 @@
-let value=0;
+// 1.Function which runs on click ( It is acting as Event handler)
 
-function counter() {
-    // value=value+1;
-    // value+=1;
-   value ++;
-    console.log(value);
+// let value=0;
+
+// function counter() {
+//     // value=value+1;
+//     // value+=1;
+//    value ++;
+//     console.log(value);
     
-};
+// };
+
+// 2.Dom Manipulation
+
+let button=document.getElementById('btn');
+button.addEventListener('click',toggle);
+
+function toggle() {
+    if (button.innerHTML==='Hi') {
+        button.innerHTML='Bye';
+    }
+   else
+    {
+        button.innerHTML='Hi';
+    }
+    
+}
